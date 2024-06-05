356a7aa1750b8f78bd78511070e18d7313923648
L0st1 <hunjin360@gmail.com>
2024-06-05 15:57:02 +0800

    feat：新增基于createGlobalState的通信
    子应用引用的store.ts最好经由主应用下发【暂未解决】

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>MricoApp__Vue3</h1>
     <el-descriptions :column="1" border>
-      <el-descriptions-item label="qiankun Action A">{{ 0 }}</el-descriptions-item>
+      <el-descriptions-item label="vueuse A">{{ a }}</el-descriptions-item>
       <el-descriptions-item label="ice/stark-data B">{{ b }}</el-descriptions-item>
       <el-descriptions-item label="子应用定时器实时刷新 VueX C">{{ c }}</el-descriptions-item>
       <el-descriptions-item label="不挂载方法或属性到实例 C">{{ c2 }}</el-descriptions-item>
@@ -16,6 +16,7 @@ import { ref, getCurrentInstance, onUnmounted } from "vue";
 import { useIntervalFn } from "@vueuse/core";
 import { ElDescriptions, ElDescriptionsItem, ElButton } from "element-plus";
 import { store as iceStore, event as iceEvent } from "@ice/stark-data";
+import { useGlobalState } from "@/vueuse/store"; 
 
 import { baseStore } from "@/main";
 
@@ -54,4 +55,6 @@ onUnmounted(() => {
   iceStore.off("iceData");
 });
 
+const { getter, setter } = useGlobalState();
+const a = getter("count");
 </script>
